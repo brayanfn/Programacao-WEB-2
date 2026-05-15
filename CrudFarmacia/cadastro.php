3ad2f925244c262f7b20a893f6450d523bc0f384
brayanfn <brayanekauan1301@gmail.com>
2026-05-15 20:33:04 -0300

    Adiciona validação de envio e obrigatoriedade no formulário
    Valida envio de dados e adiciona campos obrigatórios no formulário.

--- CrudFarmacia/cadastro.php
+++ CrudFarmacia/cadastro.php
@@ -6,43 +6,45 @@
     <title>Cadastro</title>
 </head>
 <body>
-  <h2>Cadastro de Produtos</h2>
+    <h2>Cadastro de Produtos</h2>
   
-  <form id="frmCadastro" method="post">
-    <label for="nome">Nome</label>
-    <input type="text" id="nome" name="nome">
-    <br>
-    <label for="fabricante">Fabricante</label>
-    <input type="text" id="fabricante" name="fabricante">
-    <br>
-    <label for="preco">Preço</label>
-    <input type="number" id="preco" name="preco" step="0.01">
-    <br>
-    <label for="estoque">Estoque</label>
-    <input type="number" id="estoque" name="estoque">
-    <br>
-    <button type="submit">Cadastrar</button>
-  </form>
+    <form id="frmCadastro" method="post">
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome" required>
+        <br>
+        <label for="fabricante">Fabricante</label>
+        <input type="text" id="fabricante" name="fabricante" required>
+        <br>
+        <label for="preco">Preço</label>
+        <input type="number" id="preco" name="preco" step="0.01" required>
+        <br>
+        <label for="estoque">Estoque</label>
+        <input type="number" id="estoque" name="estoque" required>
+        <br>
+        <button type="submit">Cadastrar</button>
+    </form>
 </body>
 </html>
 
 <?php
-  
-  $nome = $_REQUEST['nome'];
-  $fabricante = $_REQUEST['fabricante'];
-  $preco = $_REQUEST['preco'];
-  $estoque = $_REQUEST['estoque'];
+
+if (isset($_POST['nome'], $_POST['fabricante'], $_POST['preco'], $_POST['estoque'])) {
+    $nome = $_REQUEST['nome'];
+    $fabricante = $_REQUEST['fabricante'];
+    $preco = $_REQUEST['preco'];
+    $estoque = $_REQUEST['estoque'];
     
-  require_once('config/conexao.php');
+    require_once('config/conexao.php');
       
-  $sql = "INSERT INTO produtos (nome, fabricante, preco, estoque) VALUES (:nome, :fabricante, :preco, :estoque)";
-  $stmt = $conexao -> prepare($sql);
+    $sql = "INSERT INTO produtos (nome, fabricante, preco, estoque) VALUES (:nome, :fabricante, :preco, :estoque)";
+    $stmt = $conexao -> prepare($sql);
       
-  $stmt -> execute([
-      ':nome' => $nome,
-      ':fabricante' => $fabricante,
-      ':preco' => $preco,
-      ':estoque' => $estoque
-      ]);
+    $stmt -> execute([
+        ':nome' => $nome,
+        ':fabricante' => $fabricante,
+        ':preco' => $preco,
+        ':estoque' => $estoque
+        ]);
+}
 
 ?>
