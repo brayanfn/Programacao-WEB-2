c42c994465016f166bf70c50212ea245d4c135a8
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-05-16 17:28:33 -0300

    Correção do include do footer.

--- CrudFarmacia/index.php
+++ CrudFarmacia/index.php
@@ -10,9 +10,6 @@
         include 'includes/header.php';
     ?>
     <h2>Dados do Banco de Dados</h2>
-    <?php
-        include 'includes/footer.php';
-    ?>
 </body>
 </html>
 
@@ -49,5 +46,5 @@ if ($tabela){
     }
 }
     echo "</table>";
-    
+include 'includes/footer.php';
 ?>
