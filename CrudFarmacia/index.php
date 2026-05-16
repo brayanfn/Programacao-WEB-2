c0593f5510ff1694651f8a6174d7066113110548
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-05-15 22:10:48 -0300

    Atualização do Index.php
    Ainda seguindo o modelo citado anteriormente, a lógica deste foi aplicada no contexto do projeto.

--- CrudFarmacia/index.php
+++ CrudFarmacia/index.php
@@ -3,8 +3,20 @@
 require 'conexao.php';
 
 $sql = "Select * FROM produtos";
-$stmt = ""->prepare;
+$stmt = ""->prepare($sql);
 $stmt->execute();
+
+$tabela =  $smt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($tabela){
+    foreach ($tabela as $item){
+        echo "ID: " . $item['id'] ;
+        echo "Nome: " . $item['nome'];
+        echo "Fabricante: " . $item['fabricante'];
+        echo "Preço: " . $item['preco'];
+        echo "Estoque: " . $item['estoque'];
+    }
+}
 
 
 ?>
