2a9c7da08addca5ad4a6301766ea2f0b60992ee8
WilliamLucas835 <Williamlucasd04@gmail.com>
2026-05-16 17:56:28 -0300

    Ajuste de erros de digitação.

--- CrudFarmacia/excluir.php
+++ CrudFarmacia/excluir.php
@@ -2,26 +2,26 @@
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=,, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    <?php
         include 'includes/header.php';
     ?>
     <h2>Teste de Funcionalidade</h2>
 
     <form method = "POST" action = "">
         <input type = "number" name = "id">
-        <button type = "subimt" name = "btnExcluir">Excluir</button>
+        <button type = "submit" name = "btnExcluir">Excluir</button>
     </form>
     <?php
-        include 'includes/footer.php>;
+        include 'includes/footer.php';
     ?>
 </body>
 </html>
 
-<?php 
+<?php
 
     require_once 'config/conexao.php';
 
@@ -35,6 +35,5 @@
         $stmt->execute([':id' => $id]);
 
     }
-
 
 ?>
