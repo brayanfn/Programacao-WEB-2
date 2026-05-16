bf49effb130b21b4c186147868d80fff1309dad3
brayanfn <brayanekauan1301@gmail.com>
2026-05-15 23:43:43 -0300

    Estrutura do header adicionada
    Estrutura de classes, header, nav e itens adicionadas em header.php

---
+++ CrudFarmacia/includes/header.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <a href="#" class="header__logo">Stock Farma</a>
+    <nav class="header__nav">
+        <ul class="nav__lista">
+            <li class="nav__item">
+                <a href="index.php" class="nav__link">Catálogo</a>
+            </li>
+        </ul>
+    </nav>
+</div>
