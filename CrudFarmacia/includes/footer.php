413324c26ea671d18190a9732fb026b071e5c2e1
brayanfn <brayanekauan1301@gmail.com>
2026-05-16 21:38:42 -0300

    Correção de erros de digitação

--- CrudFarmacia/includes/footer.php
+++ CrudFarmacia/includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <p class="footer__direitos">Todos os direitos reservados &copy;</p>
-    <p class="footer__descricao">Deselvonvido por: Brayan Frezzarin, Gustavo de lima e William Lucas</p>
+    <p class="footer__descricao">Desenvolvido por: Brayan Frezzarin, Gustavo de Lima e William Lucas</p>
 </footer>
