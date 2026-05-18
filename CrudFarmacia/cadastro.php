585470b8301986892fadb03f7e7d5f5f0c0545cf
Gustavo de Lima Costa <gustavo.limacosta1804@gmail.com>
2026-05-17 22:36:33 -0300

    Adição do <main>
    Adição do <main> para facilitar a estilização no css

--- CrudFarmacia/cadastro.php
+++ CrudFarmacia/cadastro.php
@@ -6,30 +6,35 @@
     <title>Cadastro</title>
 </head>
 <body>
+    
     <?php
         include 'includes/header.php';
     ?>
+
+    <main>
+        <h2>Cadastro de Produtos</h2>
+        
+        <form id="frmCadastro" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" id="nome" name="nome" required>
+            <br>
+            <label for="fabricante">Fabricante</label>
+            <input type="text" id="fabricante" name="fabricante" required>
+            <br>
+            <label for="preco">Preço</label>
+            <input type="number" id="preco" name="preco" step="0.01" required>
+            <br>
+            <label for="estoque">Estoque</label>
+            <input type="number" id="estoque" name="estoque" required>
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
+    </main>
     
-    <h2>Cadastro de Produtos</h2>
-  
-    <form id="frmCadastro" method="post">
-        <label for="nome">Nome</label>
-        <input type="text" id="nome" name="nome" required>
-        <br>
-        <label for="fabricante">Fabricante</label>
-        <input type="text" id="fabricante" name="fabricante" required>
-        <br>
-        <label for="preco">Preço</label>
-        <input type="number" id="preco" name="preco" step="0.01" required>
-        <br>
-        <label for="estoque">Estoque</label>
-        <input type="number" id="estoque" name="estoque" required>
-        <br>
-        <button type="submit">Cadastrar</button>
-    </form>
     <?php
         include 'includes/footer.php';
     ?>
+
 </body>
 </html>
 
